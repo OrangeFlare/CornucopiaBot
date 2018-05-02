cd735ac82694db878e64d0455cccf01c3b993be8
Josef Baltz <josef@baltz.net>
2018-05-02 15:54:56 -0500

    Basics Setup
    - Configured .gitignore
    - Setup Gradle
    - Setup Dependencies
    - Bot Can Login to Discord
    - 'ping' Command Added

---
+++ src/main/java/me/orangeflare/cornucopia/botModules/coreCommands.java
@@ -0,0 +1,45 @@
+package me.orangeflare.cornucopia.botModules;
+
+import sx.blah.discord.api.events.EventSubscriber;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.util.DiscordException;
+import sx.blah.discord.util.RequestBuffer;
+import me.orangeflare.cornucopia.cornucopia;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class coreCommands {
+    public static void sendMessage(IChannel channel, String message) {
+        RequestBuffer.request(() -> {
+            try {
+                channel.sendMessage(message);
+            } catch (DiscordException error){
+                System.err.println("[ERROR] Message Failed to send: ");
+                error.printStackTrace();
+            }
+        });
+    }
+    public static String getDisplayName(MessageReceivedEvent event) { return event.getAuthor().getDisplayName(event.getGuild()); }
+    @EventSubscriber
+    private void onMessageReceived(MessageReceivedEvent event) {
+        String[] argArray = event.getMessage().getContent().split(" ");
+        if (argArray.length == 0) { return; }
+        if (!argArray[0].toLowerCase().startsWith(cornucopia.BOT_PREFIX)) { return; }
+        String command = argArray[0].substring(1).toLowerCase();
+        List<String> argsList = new ArrayList<>(Arrays.asList(argArray));
+        argsList.remove(0);
+
+        switch(command) {
+            case "ping":
+                pingCommand(event);
+                System.out.println("[INFO] 'ping' Command Received!");
+                System.out.println("       Command sent by " + getDisplayName(event));
+        }
+    }
+    private void pingCommand(MessageReceivedEvent event){
+        sendMessage(event.getChannel(), "Pong!");
+    }
+}
---
+++ src/main/java/me/orangeflare/cornucopia/cornucopia.java
@@ -0,0 +1,23 @@
+package me.orangeflare.cornucopia;
+
+import sx.blah.discord.api.ClientBuilder;
+import sx.blah.discord.api.IDiscordClient;
+import me.orangeflare.cornucopia.botModules.*;
+
+public class cornucopia {
+    public static String BOT_PREFIX = "c!";
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Error with Provided Arguments!");
+            System.out.println("  Did you forget to supply the API Token?");
+            System.out.println("  Ex: java -jar cornucopia.jar [API Token Here]");
+            return;
+        }
+        IDiscordClient bot = getBuiltDiscordClient(args[0]);
+        bot.getDispatcher().registerListener(new coreCommands());
+        bot.login();
+    }
+    public static IDiscordClient getBuiltDiscordClient(String botToken) {
+        return new ClientBuilder().withToken(botToken).build();
+    }
+}
