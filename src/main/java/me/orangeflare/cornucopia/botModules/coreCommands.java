8c406c1b5c3a8734f9b9c641b6cd5a875148f6be
Josef Baltz <josef@baltz.net>
2018-05-03 08:51:14 -0500

    Added "add" Command [UNTESTED]
    Unable to test Pushed Code due to Firewall Restrictions at School

--- src/main/java/me/orangeflare/cornucopia/botModules/coreCommands.java
+++ src/main/java/me/orangeflare/cornucopia/botModules/coreCommands.java
@@ -1,5 +1,6 @@
 package me.orangeflare.cornucopia.botModules;
 
+import sun.plugin2.message.Message;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 import sx.blah.discord.handle.obj.IChannel;
@@ -37,9 +38,31 @@ public class coreCommands {
                 pingCommand(event);
                 System.out.println("[INFO] 'ping' Command Received!");
                 System.out.println("       Command sent by " + getDisplayName(event));
+            case "add":
+                addCommand(event, argsList);
+                System.out.println("[INFO] 'add' Command Received!");
+                System.out.println("       Command sent by " + getDisplayName(event));
         }
     }
     private void pingCommand(MessageReceivedEvent event){
         sendMessage(event.getChannel(), "Pong!");
+    }
+    private void addCommand(MessageReceivedEvent event, List<String> args) throws IllegalArgumentException, IndexOutOfBoundsException {
+        if (args.size() != 2) {
+            sendMessage(event.getChannel(), "You Can Only Supply 2 Arguments!");
+            sendMessage(event.getChannel(), "    Ex: c!add 1 2");
+        } else {
+            try {
+                int number = Integer.parseInt(args.get(0) + args.get(1));
+            } catch (IllegalArgumentException e) {
+                sendMessage(event.getChannel(), "Error");
+                throw e;
+            } catch (IndexOutOfBoundsException e){
+                sendMessage(event.getChannel(), "Error");
+                throw e;
+            }
+            int number = Integer.parseInt(args.get(0) + args.get(1));
+            sendMessage(event.getChannel(), Integer.toString(number));
+        }
     }
 }
