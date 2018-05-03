5abcfe8e1e614e787e63ff3abde2f4f306dbaae0
Josef Baltz <josef@baltz.net>
2018-05-03 09:00:10 -0500

    Code Optimization
    Moved around Functions and Changed Some Function Access to Private

--- src/main/java/me/orangeflare/cornucopia/botModules/coreCommands.java
+++ src/main/java/me/orangeflare/cornucopia/botModules/coreCommands.java
@@ -1,53 +1,16 @@
 package me.orangeflare.cornucopia.botModules;
 
-import sun.plugin2.message.Message;
-import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
-import sx.blah.discord.handle.obj.IChannel;
-import sx.blah.discord.util.DiscordException;
-import sx.blah.discord.util.RequestBuffer;
-import me.orangeflare.cornucopia.cornucopia;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class coreCommands {
-    public static void sendMessage(IChannel channel, String message) {
-        RequestBuffer.request(() -> {
-            try {
-                channel.sendMessage(message);
-            } catch (DiscordException error){
-                System.err.println("[ERROR] Message Failed to send: ");
-                error.printStackTrace();
-            }
-        });
-    }
-    public static String getDisplayName(MessageReceivedEvent event) { return event.getAuthor().getDisplayName(event.getGuild()) + " (" + event.getAuthor().getName() + ")"; }
+import static me.orangeflare.cornucopia.cornucopia.sendMessage;
 
-    @EventSubscriber
-    public void onMessageReceived(MessageReceivedEvent event) {
-        String[] argArray = event.getMessage().getContent().split(" ");
-        if (argArray.length == 0) { return; }
-        if (!argArray[0].toLowerCase().startsWith(cornucopia.BOT_PREFIX)) { return; }
-        String command = argArray[0].substring(2).toLowerCase();
-        List<String> argsList = new ArrayList<>(Arrays.asList(argArray));
-        argsList.remove(0);
-        switch(command) {
-            case "ping":
-                pingCommand(event);
-                System.out.println("[INFO] 'ping' Command Received!");
-                System.out.println("       Command sent by " + getDisplayName(event));
-            case "add":
-                addCommand(event, argsList);
-                System.out.println("[INFO] 'add' Command Received!");
-                System.out.println("       Command sent by " + getDisplayName(event));
-        }
-    }
-    private void pingCommand(MessageReceivedEvent event){
+public class coreCommands {
+    public static void pingCommand(MessageReceivedEvent event){
         sendMessage(event.getChannel(), "Pong!");
     }
-    private void addCommand(MessageReceivedEvent event, List<String> args) throws IllegalArgumentException, IndexOutOfBoundsException {
+    public static void addCommand(MessageReceivedEvent event, List<String> args) throws IllegalArgumentException, IndexOutOfBoundsException {
         if (args.size() != 2) {
             sendMessage(event.getChannel(), "You Can Only Supply 2 Arguments!");
             sendMessage(event.getChannel(), "    Ex: c!add 1 2");
--- src/main/java/me/orangeflare/cornucopia/cornucopia.java
+++ src/main/java/me/orangeflare/cornucopia/cornucopia.java
@@ -3,9 +3,18 @@ package me.orangeflare.cornucopia;
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 import me.orangeflare.cornucopia.botModules.*;
+import sx.blah.discord.api.events.EventSubscriber;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.util.DiscordException;
+import sx.blah.discord.util.RequestBuffer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class cornucopia {
-    public static String BOT_PREFIX = "c!";
+    private static String BOT_PREFIX = "c!";
     public static void main(String[] args) {
         if (args.length != 1) {
             System.err.println("Error with Provided Arguments!");
@@ -14,10 +23,41 @@ public class cornucopia {
             return;
         }
         IDiscordClient bot = getBuiltDiscordClient(args[0]);
+        bot.getDispatcher().registerListener(new cornucopia());
         bot.getDispatcher().registerListener(new coreCommands());
         bot.login();
     }
     private static IDiscordClient getBuiltDiscordClient(String botToken) {
         return new ClientBuilder().withToken(botToken).build();
+    }
+    public static void sendMessage(IChannel channel, String message) {
+        RequestBuffer.request(() -> {
+            try {
+                channel.sendMessage(message);
+            } catch (DiscordException error){
+                System.err.println("[ERROR] Message Failed to send: ");
+                error.printStackTrace();
+            }
+        });
+    }
+    public static String getDisplayName(MessageReceivedEvent event) { return event.getAuthor().getDisplayName(event.getGuild()) + " (" + event.getAuthor().getName() + ")"; }
+    @EventSubscriber
+    public static void onMessageReceived(MessageReceivedEvent event) {
+        String[] argArray = event.getMessage().getContent().split(" ");
+        if (argArray.length == 0) { return; }
+        if (!argArray[0].toLowerCase().startsWith(BOT_PREFIX)) { return; }
+        String command = argArray[0].substring(2).toLowerCase();
+        List<String> argsList = new ArrayList<>(Arrays.asList(argArray));
+        argsList.remove(0);
+        switch(command) {
+            case "ping":
+                coreCommands.pingCommand(event);
+                System.out.println("[INFO] 'ping' Command Received!");
+                System.out.println("       Command sent by " + getDisplayName(event));
+            case "add":
+                coreCommands.addCommand(event, argsList);
+                System.out.println("[INFO] 'add' Command Received!");
+                System.out.println("       Command sent by " + getDisplayName(event));
+        }
     }
 }
